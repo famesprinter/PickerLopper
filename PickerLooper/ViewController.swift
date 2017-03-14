81522d75d53ea9368b95c068b67051b205391428
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2017-03-14 14:15:45 +0700

    - Create looper picker view 0 to 99.

--- PickerLooper/ViewController.swift
+++ PickerLooper/ViewController.swift
@@ -12,10 +12,19 @@ class ViewController: UIViewController {
     // MARK: - IBOutlet
     @IBOutlet weak var pickerView: UIPickerView!
     
+    // MARK: - Variable
+    var datas = [Int]()
+    
     // MAKR: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        for index in 0...99 {
+            datas.append(index)
+        }
+        
+        print("COUNT: \(datas.count)")
+        pickerView.selectRow((1000/2), inComponent: 0, animated: false)
     }
 }
 
@@ -26,16 +35,10 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
     func pickerView(_ pickerView: UIPickerView,
                     numberOfRowsInComponent component: Int) -> Int {
-        return 100
+        return 1000
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "TEST"
-    }
-    
-    func pickerView(_ pickerView: UIPickerView,
-                    didSelectRow row: Int,
-                    inComponent component: Int) {
-        
+        return String(datas[row%100])
     }
 }
